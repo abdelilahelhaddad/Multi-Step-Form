bb3fa35e29fe0a5c3ab982303f8b445554fa4ee2
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2023-02-09 18:22:34 +0100

    :fire: :art: Steps Functionality :sparkles: :globe_with_meridians:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const goBack = document.querySelector('.goBack')
 const nextStep = document.querySelector('.nextStep')
+const confirmStep = document.querySelector('.confirmStep')
 const circles = document.querySelectorAll('.circle')
 const theContents = document.querySelectorAll('.theContent')
 
@@ -26,8 +27,8 @@ goBack.addEventListener('click', () => {
 
 
 function update() {
-  circles.forEach((circle, idx) => {
-    if (idx < currentActive) {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
       for (var i = 0; i < circles.length; i++) {
         circles[i].classList.remove('activeCircle');
       }
@@ -35,12 +36,23 @@ function update() {
     }
   })
 
+  theContents.forEach((theContent, index) => {
+    if (index < currentActive) {
+      for (var i = 0; i < theContents.length; i++) {
+        theContents[i].classList.add('hidden');
+      }
+      theContent.classList.remove('hidden')
+    }
+  })
+
   if (currentActive === 1) {
     goBack.classList.add('visibility')
   } else if (currentActive === circles.length) {
     nextStep.classList.add('visibility')
+    confirmStep.classList.remove('hidden')
   } else {
     goBack.classList.remove('visibility')
     nextStep.classList.remove('visibility')
+    confirmStep.classList.add('hidden')
   }
 }
